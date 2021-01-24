d2774a305c6675929b7e829a7b5c4175328e565b
keelim <kimh00335@gmail.com>
2021-01-24 22:14:21 +0900

    20210124

---
+++ app/src/main/java/com/keelim/architecure/di/AppComponent.java
@@ -0,0 +1,20 @@
+package com.keelim.architecure.di;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Singleton
+@Component(modules={
+        AndroidSupportInjectionModule.class,
+        ActivityModule.class,
+        AppModule.class
+})
+public interface AppComponent extends AndroidInjector<App>{
+    @Component.Factory
+    abstract class Factory implements AndroidInjector.Factory<App>{
+
+    }
+}
