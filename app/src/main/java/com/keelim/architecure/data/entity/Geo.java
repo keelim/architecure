bdf495da31e094cdc6008f8d49bd30d81791b18a
keelim <kimh00335@gmail.com>
2021-01-24 22:05:23 +0900

    gitflow-feature-stash: 20210124

---
+++ app/src/main/java/com/keelim/architecure/data/entity/Geo.java
@@ -0,0 +1,14 @@
+package com.keelim.architecure.data.entity;
+
+public class Geo {
+    private String lat;
+    private String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+}
